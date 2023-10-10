a214acd545dafc2cc8546e47a80adf35f4bf7160
Kumar Kartikay Dwivedi <kumar@pipesort.com>
2023-10-10 12:22:33 +0530

    chore: removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
-
+import { Session, getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  const session: Session | null = await getServerSession(authOptions)
+
+  // if (session?.user) {
+  //   redirect('/home')
+  // } else {
+  //   redirect('/auth/sign-in')
+  // }
+
+  return (
+    <div>
+      <p>This is main page</p>
+      {/* <div>{JSON.stringify(data)}</div>  */}
+    </div>
+  )
 }
+
+// export default async function PrivateLayout({ children }: Props) {
+//   const session = await getServerSession(authOptions)
+//   if (!session?.user) redirect('/auth/sign-in')
+//   return <>{children}</>
+// }
